f4a4767a4aa3038b4172a3fd014cd5c57e6d455b
AnantapalliJayasri <78068181+AnantapalliJayasri@users.noreply.github.com>
2023-07-13 12:28:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,76 @@
+//import logo from './logo.svg';
+import './App.css';
+import {React} from 'react';
+import Sample from './components/Sample.js';
+import BasicWebpage from './components/BasicWebpage';
+//import {Link,Routes,Route} from 'react-router-dom';
+ import List from './components/List';
+import Props from './components/Props';
+import Hooks from './components/Hooks';
+import HttpGetReq from './components/HttpGetReq';
+import Search from './components/Search';
+import HooksCounter from './components/HooksCounter';
+import Hooks_Webpage from './components/Hooks_Webpage';
+import UseeffectHook from './components/UseeffectHook';
+import TypographyEx from './reactmui/TypographyEx';
+import Buttons from './reactmui/Buttons';
+import Togglebuttons from './reactmui/Togglebuttons';
+import TextFields from './reactmui/TextFields';
+import WebPage from './reactmui/WebPage';
+import {Box, Card} from '@mui/material'
+import Selects from './reactmui/Selects';
+import Radiobuttons from './reactmui/Radiobuttons';
+import Checkboxs from './reactmui/Checkboxs';
+import Navbar from './reactmui/Navbar';
+import Cards from './reactmui/Cards';
+import Tables from './reactmui/Tables';
+import Treeview from './reactmui/Treeview';
+import Routers from './reactmui/Routers';
+import Links from './reactmui/Links';
+import TreeView from '@mui/lab/TreeView';
+import Images from './reactmui/Images';
+
+function App() {
+    return (
+          
+     
+        // <BasicWebpage/> 
+       // <Hooks_Webpage/>
+
+       //<UseeffectHook/>
+      //<List/>
+    
+     /*props example where we can retreive tags also using props.children in child component.
+    <div>
+         <Props name="jayasri" color="pink">
+               <button>Click</button>
+          </Props>
+     </div>*/
+    // <HttpGetReq/>
+     //<Search/>
+
+    //<TypographyEx/>
+    //<Buttons/>
+    //<Togglebuttons/>
+    //<TextFields/>
+    //<WebPage/>
+     //<Selects/>
+     //<Radiobuttons/>
+     //<Checkboxs/>
+     //<Navbar/>
+    //<Cards/>
+    //<Tables/>
+    //<Routers/>
+      <Treeview/>
+     //<Links/>
+     //<Images/>
+
+     // <div>
+     //      <Links/>
+     //      <Treeview/>
+     // </div>
+
+     );
+}
+
+export default App;
